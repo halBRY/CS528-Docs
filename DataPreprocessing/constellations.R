517a25fbcf7249599705977975c7373470b01b73
HAL <z1842318@students.niu.edu>
2024-04-08 00:02:22 -0500

    Adding pages

--- DataPreprocessing/constellations.R
+++ DataPreprocessing/constellations.R
@@ -1,21 +1,22 @@
 library(dplyr) 
 library(tidyverse) 
 
-stars <- read.csv("E:/School/UIC/23-24/CS 528/CS528-Project/Assets/Data/athyg_v31-1_cleaned.csv")
-shapes <- readLines("E:/School/UIC/23-24/CS 528/DataProcessing/constellationship.fab")
+stars <- read.csv("E:/School/UIC/23-24/CS 528/CS528-Project/Assets/Resources/athyg_v31_cleaned.csv")
+shapes <- readLines("E:/School/UIC/23-24/CS 528/DataProcessing/constellationship_zulu.fab")
 
-shapes <- shapes[-c(89, 90), ] 
+#shapes <- shapes[-c(89,90)] 
 
 # Create a smaller loop for testing
 #shapes <- shapes[c(0,1)]
 
 all_constellations <- list()
 counter <- 0
+const_num <- 0
 
 # For each constellation, save the (x,y,z) coords for each star as pairs
 for(constellation in shapes)
 {
-  myConstellation <- ""
+  myConstellation <- const_num
   
   #Get all stars from line
   data <- scan(text = constellation[1], what = "")
@@ -35,19 +36,24 @@ for(constellation in shapes)
     #myCoords <- myStar[c("x0_feet", "y0_feet", "z0_feet")]
     #print(myCoords)
   
-    xCoord <- myStar["x0_feet"]
-    yCoord <- myStar["y0_feet"]
-    zCoord <- myStar["z0_feet"]
+    xCoord <- myStar["x0"]
+    yCoord <- myStar["y0"]
+    zCoord <- myStar["z0"]
     
-    myCoords <- c(xCoord, yCoord, zCoord)
+    vxCoord <- myStar["vx"]
+    vyCoord <- myStar["vy"]
+    vzCoord <- myStar["vz"]
+    
+    myCoords <- c(xCoord, yCoord, zCoord, vxCoord, vyCoord, vzCoord)
 
     myConstellation <- paste(myConstellation, paste(myCoords, collapse=","), sep=" ") 
     counter <- counter + 1
   }
+  const_num <- const_num + 1
   
   all_constellations <- append(all_constellations, myConstellation, after = length(all_constellations))
 }
 
 print(counter)
 
-lapply(all_constellations, write, "E:/School/UIC/23-24/CS 528/CS528-Project/Assets/Data/constellations.txt", append=T, ncolumns=length(all_constellations))
+lapply(all_constellations, write, "E:/School/UIC/23-24/CS 528/DataProcessing/constellationship_zulu.txt", append=T, ncolumns=length(all_constellations))
